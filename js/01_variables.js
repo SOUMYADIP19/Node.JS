ffb8a29dfac918504dad3a64c3dae859cab2dd9c
SOUMYADIP19 <79806550+SOUMYADIP19@users.noreply.github.com>
2024-11-26 19:00:35 +0530

    js

---
+++ js/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail ="rijusoumyadip1999@gmail.com"
+var accountPassword = "123456"
+accountCity = "Kolkata"
+
+// accountId=5 not allowed
+
+accountEmail = "hakkanoodles69@gmail.com"
+accountPassword = "779762"
+accountCity = "Delhi"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+*/
+
+console.table ([accountId,accountEmail,accountPassword,accountCity])
+
+
